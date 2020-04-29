5e97d802538ed0229132caad1598d36e489e94c8
Mikael Krief <mkrief@talentsoft.com>
2020-04-30 00:48:15 +0200

    update

--- CHAP03/list_map/outputs.tf
+++ CHAP03/list_map/outputs.tf
@@ -1,3 +1,7 @@
 output "app_service_names" {
   value = [for x in azurerm_app_service.app : x.name]
 }
+
+output "app_service_urls" {
+  value = {for x in azurerm_app_service.app :  x.name => x.default_site_hostname  }
+}
