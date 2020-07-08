db0d243861c0c6db92bc95ffe21113783ed737a5
Mikael Krief <mkrief@talentsoft.com>
2020-07-08 21:53:03 +0200

    update

---
+++ CHAP07/ansible-inventory/main.tf
@@ -0,0 +1,58 @@
+variable "virtual_machines" {
+  default = [
+    {
+      dns        = "test1.test.cloud"
+      index      = "01"
+      address_ip = "0.0.0.1"
+    }
+  ]
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+variable "vmhosts" {
+  type    = list(string)
+  default = ["vmwebdemo1", "vmwebdemo2"]
+}
+
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = "rg-demoinventory"
+  subnet_prefixes     = ["10.0.2.0/24"]
+  subnet_names        = ["subnet1"]
+}
+
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  resource_group_name = "rg-demoinventory"
+  vm_os_simple        = "UbuntuServer"
+  nb_instances        = 2
+  nb_public_ip        = 2
+  vm_hostname         = "vmwebdemo"
+  public_ip_dns       = var.vmhosts // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+}
+
+
+
+resource "local_file" "inventory" {
+  depends_on = [module.linuxservers]
+  filename   = "inventory"
+  content = templatefile("template-inventory.tpl",
+    {
+      vm_ip  = module.linuxservers.network_interface_private_ip
+      vm_dns = var.vmhosts
+  })
+
+}
+
+output "ips" {
+  value = module.linuxservers.public_ip_address
+}
+
+output "dns" {
+  value = module.linuxservers.public_ip_dns_name
+}
